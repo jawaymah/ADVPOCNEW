b86b77fd2bd978be1fee4aa15c60618ed90b47c1
jawaymah <104241252+jawaymah@users.noreply.github.com>
2024-06-30 18:30:16 +0300

    add conversion manager and convert to detail button

--- AddinSetup/App.cs
+++ AddinSetup/App.cs
@@ -46,7 +46,7 @@ namespace AdvansysPOC
                 Autodesk.Revit.UI.TaskDialog.Show("Error", ex.Message);
                 return Result.Failed;
             }
-            
+
             a.ControlledApplication.DocumentChanged += ControlledApplication_DocumentChanged;
 
 
@@ -171,6 +171,14 @@ namespace AdvansysPOC
                 //                    "dimension32.png"), UriKind.Absolute));
                 //buttonDataDimensions.LargeImage = new BitmapImage(new Uri(Path.Combine(@"C:\Users\Jemmy\source\repos\AdvansysPOC\AdvansysPOC\Resources", "add32.png"), UriKind.Absolute));
                 Autodesk.Revit.UI.RibbonItem flipButton = panel.AddItem(buttonFlip);
+
+                PushButtonData buttonConvert = new PushButtonData("ConvertToDetail", "Convert To Detail", Assembly.GetExecutingAssembly().Location, "AdvansysPOC.ConvertToDetailCommand");
+                //                buttonDataDimensions.LargeImage = new BitmapImage(new Uri(Path.Combine(UIConstants.ButtonIconsFolder,
+                //"dimension32.png"), UriKind.Absolute));
+                //                buttonDataDimensions.Image = new BitmapImage(new Uri(Path.Combine(UIConstants.ButtonIconsFolder,
+                //                    "dimension32.png"), UriKind.Absolute));
+                //buttonDataDimensions.LargeImage = new BitmapImage(new Uri(Path.Combine(@"C:\Users\Jemmy\source\repos\AdvansysPOC\AdvansysPOC\Resources", "add32.png"), UriKind.Absolute));
+                Autodesk.Revit.UI.RibbonItem convertButton = panel.AddItem(buttonConvert);
             }
             if (panelName == "Supports")
             {
